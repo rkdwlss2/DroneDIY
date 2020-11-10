1d2aa6111a807f439d019b704fe1a228f7c38290
chohoseong <whghtjd9500@gmsil.com>
2020-11-10 18:21:08 +0900

    demo2

---
+++ demo2/src/main/java/com/example/demo/Demo2Application.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class Demo2Application {
+	
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!0";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Demo2Application.class, args);
+	}
+
+}
